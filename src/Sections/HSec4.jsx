c90dc0287553d3e274bb680d21cdb66cad152788
Yahyobek <caisophiya@gmail.com>
2024-09-17 22:02:15 +0200

    yahyobek

--- src/Sections/HSec4.jsx
+++ src/Sections/HSec4.jsx
@@ -6,11 +6,11 @@ const HSec4 = () => {
     <div className="w-[100%] sm:h-[70vh] h-[55vh] flex items-center justify-center bg-white rounded-t-2xl border-2 shadow-xl sm:mt-0 mt-4">
       <div className="w-[50%] sm:h-[50%] h-[100%]">
         <div className='w-[100%] flex justify-center'>
-          <img src={agreementImg} alt="agree" className='w-[150px] h-[150px] rounded-full shadow-lg' />
+          <img src={agreementImg} alt="agree" className='w-[150px] h-[150px] rounded-full shadow-xl' />
         </div>
         <h3 className='text-center mt-5 uppercase sm:text-3xl text-xl font-bold'>tell me about your next project</h3>
         <div className='flex mt-8 w-[100%] justify-evenly'>
-          <Link to="/" className='py-3 px-6 bg-gradient-to-tl from-greens to-kok border-b-2 border-slate-500 text-white rounded-lg hover:opacity-85'>Contact me</Link>
+          <Link to="/" className='py-3 px-6 bg-gradient-to-tl from-greens to-kok text-white rounded-lg hover:opacity-85 active:mt-2 border-b-2 border-green-600'>Contact me</Link>
         </div>
       </div>
     </div>
