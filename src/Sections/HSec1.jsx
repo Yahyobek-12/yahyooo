af283a3cb7929c5a6451a3c572e8971973a24b03
Yahyobek <caisophiya@gmail.com>
2024-09-17 22:07:57 +0200

    yahyobek

--- src/Sections/HSec1.jsx
+++ src/Sections/HSec1.jsx
@@ -1,6 +1,6 @@
 const HSec1 = () => {
   return (
-    <div className="w-[100%] sm:h-[82vh] h-[60vh] HSec1 lg:px-20 md:px-16 px-4 py-2 lg:mb-0 md:mb-12 mb-0 bg-red-500">
+    <div className="w-[100%] sm:h-[82vh] h-[40vh] HSec1 lg:px-20 md:px-16 px-4 py-2 lg:mb-0 md:mb-12 mb-0 bg-red-500">
       <h1 className="lg:text-7xl md:text-6xl sm:text-5xl ss:text-3xl text-2xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error repudiandae nisi voluptatum, sequi ex tempora culpa, sint provident labore ipsa a nostrum. At ipsam sunt unde commodi veritatis, ratione, dignissimos eligendi pariatur et</h1>
     </div>
   )
