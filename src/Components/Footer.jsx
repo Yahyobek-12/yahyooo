983971737ee4adf68d1f10dbf4056ec00e53d26e
Yahyobek <caisophiya@gmail.com>
2024-09-18 05:54:01 +0200

    add footer page and HSec4 page

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
         <h1>&copy; 2024 All right reserved</h1>
         <div className='ss:w-[100px] w-[100%] ss:mt-0 mt-4 flex items-center justify-center'>
           <div className='flex items-center'>
-            <Link to="/" target='_blank'><FaGithub className='text-xl ml-2 mr-2' /></Link>
+            <Link to="https://github.com/Yahyobek-12" target='_blank'><FaGithub className='text-xl ml-2 mr-2' /></Link>
             <h1 className='font-bold text-xl'>|</h1>
-            <Link to="/" target='_blank'><FaYoutube className='text-xl hover:text-red-500 ml-2 mr-2' /></Link>
+            <Link to="https://www.youtube.com/@yahyobek_12" target='_blank'><FaYoutube className='text-xl hover:text-red-500 ml-2 mr-2' /></Link>
             <h1 className='font-bold text-xl'>|</h1>
             <Link to="https://www.linkedin.com/in/yahyobek-ergashboyev-9899832a8/" target='_blank'><FaLinkedin className='text-xl hover:text-blue-500 ml-2 mr-2' /></Link>
           </div>
